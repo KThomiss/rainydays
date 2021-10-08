7f92b3bce356d658875d04e7ad2f9b5ba8cde2c0
KThomiss <79108090+KThomiss@users.noreply.github.com>
2021-10-08 22:10:41 +0200

    update

--- js/listofjackets.js
+++
@@ -1,5 +0,0 @@
-/*const activePage = document.querySelector(".active");
-
-function colorChange(activePage) {
-    activePage.document.style.backgroundColor = "red";
-}*/
---
+++ js/message.js
@@ -0,0 +1,6 @@
+const addToCart = document.querySelector(".cart");
+const message = document.querySelector(".message");
+
+addToCart.onclick = function() {
+    message.style.display = "block";
+}
