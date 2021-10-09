a234761dd1a881aebdd10a95be64d95a8c0af0cf
KThomiss <79108090+KThomiss@users.noreply.github.com>
2021-10-09 20:14:03 +0200

    contact form + language update

---
+++ js/contact.js
@@ -0,0 +1,37 @@
+const contactForm = document.querySelector("#contactForm");
+const fullName = document.querySelector("#fullName");
+const fullNameError = document.querySelector("#fullNameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+
+function validateContactForm() {
+    event.preventDefault();
+
+    if (checkLength(fullName.value, 0) === true) {
+        fullNameError.style.display = "none";
+    } else {
+        fullNameError.style.display = "block";
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+    }
+}
+
+contactForm.addEventListener("submit", validateContactForm);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const emailRequirement = regEx.test(email);
+    return emailRequirement
+}
